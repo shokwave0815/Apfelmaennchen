503d7f7b6538840b4be750cc55ff0f17a541729a
shoKwave <shokwave@gmx.de>
2026-05-04 06:39:25 +0200

    cleanup mabdelbrot.pas

--- mandelbrot.pas
+++ mandelbrot.pas
@@ -32,18 +32,10 @@ type
     function CalculateColor(const AIterations: QWord): TColor;
   public
     property OffsetX: integer read FOffsetX;
-    property Width: integer read FWidth;
-    property Height: integer read FHeight;
-    property StartReal: extended read FStartReal;
-    property StartImagenary: extended read FStartImagenary;
-    property Zoom: QWord read FZoom write FZoom;
-    property MaxIterations: QWord read FMaxIterations write FMaxIterations;
     constructor Create(const AOffsetX: integer; const AWidth: integer; const AHeight: integer;
       const AZoom: QWord; const AMaxIterations: QWord);
     destructor Destroy; override;
-    procedure SetSize(const AWidth: integer; const AHeight: integer);
     procedure SetStartPoint(const AReal: extended; const AImagenary: extended);
-    procedure ZoomInOrOut(const AFactor: double);
     procedure Calulate; virtual;
     function GetBitmap: TBitmap;
   end;
@@ -116,39 +108,10 @@ begin
   inherited Destroy;
 end;
 
-procedure TMandelbrot.SetSize(const AWidth: integer; const AHeight: integer);
-begin
-  FWidth := AWidth;
-  FHeight := AHeight;
-  FBitmap.SetSize(AWidth, AHeight);
-end;
-
 procedure TMandelbrot.SetStartPoint(const AReal: extended; const AImagenary: extended);
 begin
   FStartReal := AReal;
   FStartImagenary := AImagenary;
-end;
-
-procedure TMandelbrot.ZoomInOrOut(const AFactor: double);
-var
-  newYValue: extended;
-  oldYValue: extended;
-  newXValue: extended;
-  oldXValue: extended;
-begin
-  oldXValue := FWidth / FZoom;
-  oldYValue := FHeight / FZoom;
-
-  if AFactor > 0 then
-    FZoom := Trunc(FZoom * AFactor)
-  else
-    FZoom := Trunc(FZoom / Abs(AFactor));
-
-  newXValue := FWidth / FZoom;
-  newYValue := FHeight / FZoom;
-
-  FStartReal := FStartReal + (oldXValue - newXValue) / 2;
-  FStartImagenary := FStartImagenary + (oldYValue - newYValue) / 2;
 end;
 
 procedure TMandelbrot.Calulate;
